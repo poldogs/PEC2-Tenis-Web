529af4f72bc992d9ee341b367c6688558aa38320
[Leopoldo Gallardo Sedeño] <122998221+poldogs@users.noreply.github.com>
2023-11-25 09:46:19 +0100

    commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,34 +5,20 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
 /**
  * Write any other JavaScript below
  */
 
-import * as bootstrap from 'bootstrap'
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the current page URL
-    var currentPage = window.location.pathname;
 
-    // Set the active section based on the current page
-    setActiveSection(currentPage);
-});
 
-function setActiveSection(currentPage) {
-    // Get all the navigation links
-    var navLinks = document.querySelectorAll(".navbar-nav a");
 
-    // Iterate through each link and check if it corresponds to the current page
-    navLinks.forEach(function (link) {
-        // Remove the 'active' class from all links
-        link.classList.remove("active");
+//Cargar script de FontAwesome
+const fontAwesomeScript = document.createElement('script');
 
-        // Check if the link's href attribute matches the current page
-        if (link.getAttribute("href") === currentPage) {
-            // Add the 'active' class to the matching link
-            link.classList.add("active");
-        }
-    });
-}
+fontAwesomeScript.src = 'https://kit.fontawesome.com/63639a8bc8.js';
+fontAwesomeScript.crossOrigin = 'anonymous';
+
+document.body.appendChild(fontAwesomeScript);
